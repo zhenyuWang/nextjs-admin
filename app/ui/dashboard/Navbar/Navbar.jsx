4d1b4ab88391d44a1a1cafd22ad8c49dab6039c8
zhenyuWang <13641039885@163.com>
2023-12-26 10:59:28 +0800

    feat: Navbar

--- app/ui/dashboard/Navbar/Navbar.jsx
+++ app/ui/dashboard/Navbar/Navbar.jsx
@@ -1,5 +1,36 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import classes from './navbar.module.css'
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from 'react-icons/md'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  const pathname = usePathname()
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.title}>{pathname.split('/').pop()}</div>
+      <div className={classes.menu}>
+        <div className={classes.search}>
+          <MdSearch />
+          <input
+            type='text'
+            placeholder='Search...'
+            className={classes.input}
+          />
+        </div>
+        <div className={classes.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
