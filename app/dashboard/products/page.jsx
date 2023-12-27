55b42d1b8b3485087ebbe7eb3af5b984637b0e2b
zhenyuWang <13641039885@163.com>
2023-12-27 10:39:25 +0800

    feat: Products Page

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,5 +1,67 @@
-const ProductsPage = () => {
-  return <div>ProductsPage</div>
+import Image from 'next/image'
+import Link from 'next/link'
+import classes from '@/app/ui/dashboard/products/products.module.css'
+import Search from '@/app/ui/dashboard/search/search'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
+
+const ProductsPage = async () => {
+  const page = 1
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.top}>
+        <Search placeholder='Search for a product...' />
+        <Link href='/dashboard/products/add'>
+          <button className={classes.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={classes.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={classes.product}>
+                <Image
+                  src='/no-product.jpg'
+                  alt=''
+                  width={40}
+                  height={40}
+                  className={classes.productImage}
+                />
+                product-1
+              </div>
+            </td>
+            <td>product-desc</td>
+            <td>100</td>
+            <td>2023.12.28</td>
+            <td>200</td>
+            <td>
+              <div className={classes.buttons}>
+                <Link href={`/dashboard/products/1`}>
+                  <button className={`${classes.button} ${classes.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${classes.button} ${classes.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination count={10} />
+    </div>
+  )
 }
 
 export default ProductsPage
