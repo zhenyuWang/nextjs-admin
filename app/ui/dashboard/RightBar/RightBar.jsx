09b199937335eeb0fd876ebdb4a1232eb734691d
zhenyuWang <13641039885@163.com>
2024-01-03 11:42:23 +0800

    refactor(RightBar): css to tailwind

--- app/ui/dashboard/RightBar/RightBar.jsx
+++ app/ui/dashboard/RightBar/RightBar.jsx
@@ -1,41 +1,41 @@
-import classes from './rightBar.module.css'
 import Link from 'next/link'
+import { Button } from '@nextui-org/react'
 import { MdArrowForward } from 'react-icons/md'
 
-const Rightbar = () => {
+const RightBar = () => {
   return (
-    <div className={classes.container}>
-      <div className={classes.item}>
-        <div className={classes.text}>
-          <span className={classes.notification}>Who am I?</span>
-          <h3 className={classes.title}>I am a front-end developer.</h3>
-          <p className={classes.desc}>
+    <div className='fixed'>
+      <div className='relative mb-5 py-5 px-6 rounded-lg bg-gradient-to-t from-[var(--bgSoft)] to-[#253352]'>
+        <div className='flex flex-col gap-6'>
+          <span className='font-bold'>Who am I?</span>
+          <h3>I am a front-end developer.</h3>
+          <p className='text-[var(--textSoft)]'>
             I&apos;m a skilled software developer with experience in TypeScript
             and JavaScript, and expertise in frameworks like Vue and React.
           </p>
-          <Link
-            className={classes.button}
-            href='https://3d-personal-profile.vercel.app/'>
-            More <MdArrowForward />
+          <Link target='_blank' href='https://3d-personal-profile.vercel.app/'>
+            <Button color='primary' endContent={<MdArrowForward />}>
+              More
+            </Button>
           </Link>
         </div>
       </div>
-      <div className={classes.item}>
-        <div className={classes.text}>
-          <span className={classes.notification}>Job intention</span>
-          <h3 className={classes.title}>
-            I&apos;m looking for a front-end remote development job.
-          </h3>
-          <p className={classes.desc}>
+      <div className='relative mb-5 py-5 px-6 rounded-lg bg-gradient-to-t from-[var(--bgSoft)] to-[#253352]'>
+        <div className='flex flex-col gap-6'>
+          <span className='font-bold'>Job intention</span>
+          <h3>I&apos;m looking for a front-end remote development job.</h3>
+          <p className='text-[var(--textSoft)]'>
             I&apos;m a quick learner and collaborate closely with clients to
             create efficient, scalable, and user-friendly solutions that solve
             real-world problems. Let&apos;s work together to bring your ideas to
             life!
           </p>
           <Link
-            className={classes.button}
+            target='_blank'
             href='https://3d-personal-profile.vercel.app/#contact'>
-            Contact Me <MdArrowForward />
+            <Button color='primary' endContent={<MdArrowForward />}>
+              Contact Me
+            </Button>
           </Link>
         </div>
       </div>
@@ -43,4 +43,4 @@ const Rightbar = () => {
   )
 }
 
-export default Rightbar
+export default RightBar
