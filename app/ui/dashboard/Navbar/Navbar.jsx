6fdaf49c35148eb390000a3553e2dcf27ea35325
zhenyuWang <13641039885@163.com>
2024-01-03 11:14:03 +0800

    refactor(Navbar): add breadcrumbs & css to tailwind

--- app/ui/dashboard/Navbar/Navbar.jsx
+++ app/ui/dashboard/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
 'use client'
 import { usePathname } from 'next/navigation'
-import classes from './navbar.module.css'
 import Search from '@/app/ui/dashboard/Search/Search'
 import { MdNotifications, MdOutlineChat, MdPublic } from 'react-icons/md'
+import { Breadcrumbs, BreadcrumbItem } from '@nextui-org/react'
 
 const Navbar = () => {
   const pathname = usePathname()
-
+  const pathnameArr = pathname.substring(1).split('/')
   return (
-    <div className={classes.container}>
-      <div className={classes.title}>{pathname.split('/').pop()}</div>
-      <div className={classes.menu}>
+    <div className='p-5 flex items-center justify-between round-lg bg-[var(--bgSoft)]'>
+      <Breadcrumbs
+        color='primary'
+        size='lg'
+        itemClasses={{
+          item: ['text-slate-200'],
+          separator: 'text-slate-500',
+        }}>
+        {pathnameArr.map((item, index) => (
+          <BreadcrumbItem
+            key={index}
+            href={`/${pathnameArr.slice(0, index + 1).join('/')}`}>
+            {item}
+          </BreadcrumbItem>
+        ))}
+      </Breadcrumbs>
+      <div className='flex items-center'>
         <Search placeholder='Search...' />
-        <div className={classes.icons}>
+        <div className='ml-4 flex gap-5'>
           <MdOutlineChat size={20} />
           <MdNotifications size={20} />
           <MdPublic size={20} />
